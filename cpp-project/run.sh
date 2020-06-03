0293ee36c971168e6aac6e5e5ac22350d3a6c5c7
Ricardo Menotti <menotti@gmail.com>
2020-06-03 17:58:46 -0300

    Update run.sh

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -5,9 +5,7 @@ sample=$1
 mkdir build
 cd build
 
-cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DCOMPUTECPP_PACKAGE_ROOT_DIR="/usr/local/computecpp" -DSAMPLE_NAME=$sample > /dev/null
-
-export COMPUTECPP_TARGET=cpu # host
+cmake ../ -GNinja -DCMAKE_CXX_COMPILER="/usr/bin/g++" -DOpenCL_INCLUDE_DIR="$OCL_INC" -DOpenCL_LIBRARY="$OCL_LIB/libOpenCL.so" -DSAMPLE_NAME=$sample > /dev/null
 
 ninja $sample
 
