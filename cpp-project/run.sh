60db0b2e9fed9438666f139ba9bc1260f85e79fc
Ricardo Menotti <menotti@gmail.com>
2020-06-05 14:47:05 -0300

    Update run.sh

--- cpp-project/run.sh
+++ cpp-project/run.sh
@@ -4,6 +4,6 @@ sample=$1
 
 cd /project/target/src/exercises/
 
-clang++ -Wno-deprecated ${sample}.cpp -o $sample -I${OCL_INC} -lOpenCL -L${OCL_LIB} -I. 
+g++ -Wno-deprecated ${sample}.cpp -o $sample -I${OCL_INC} -lOpenCL -L${OCL_LIB} -I. 
 
 ./$sample
